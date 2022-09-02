95ec5860d5f591b9550630157b9840f341a660e5
Alaikum <carangiadamo@gmail.com>
2022-09-02 15:32:31 +0200

    first Commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        jumbotron
+
+
+    </div>
+</template>
+    
+    
+    <script>
+export default {
+    name: 'NewHero',
+
+}
+
+</script>
+        
+        <!-- Add "scoped" attribute to limit CSS to this component only -->
+        <style scoped lang="scss">
+        .container {
+            color: cyan;
+            max-width: 700px;
+            height: 500px;
+            background-color: black;
+            margin: 0 auto;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 3rem;
+            text-transform: uppercase;
+        
+        }
+        </style>
+        
